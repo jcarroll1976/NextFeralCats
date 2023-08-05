30ca29c50a9e73899468833829237a81ecec6015
Joshua Carroll <carrolljosh1976@gmail.com>
2023-08-05 13:26:17 -0400

    Added SocialLinks compnent and Menu to Navbar

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import CatIcon from "public/assets/cat_icon.png";
+import SocialLinks from './SocialLinks';
+
 
 type Props = {}
 
 function Navbar({}: Props) {
   return (
-    <div>Navbar</div>
+    <nav className='flex justify-between items-center w-full px-10 py-4'>
+        <div className='hidden sm:block'>
+            <SocialLinks isDark/>
+        </div>
+       <div className='flex justify-center items-center gap-1'>
+        <Image
+        src={CatIcon}
+        alt="Cat Icon"
+        height={40}
+        width={40}
+        className='fill-white'
+        />
+        <h1 className='text-2xl font-bold'>Friends of Feral Felines</h1>
+      </div>
+      <div className='flex justify-between items-center gap-10'>
+                <Link href="/">Home</Link>
+                <Link href="/">About Us</Link>
+                <Link href="/">Contact Us</Link>
+            </div>
+    </nav>
   )
 }
 
