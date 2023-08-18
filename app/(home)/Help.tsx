e5cbc138b22245e052287a269536948fdf1f41b2
Joshua Carroll <carrolljosh1976@gmail.com>
2023-08-17 21:51:16 -0400

    added a few more images to Help component

--- app/(home)/Help.tsx
+++ app/(home)/Help.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import OutdoorCat from "../../public/assets/outdoorcat.jpg"
+import CalicoKitten from "../../public/assets/CalicoKitten.jpg"
+import FoliageCat from "../../public/assets/CatinFoliage.jpg"
 import Image from 'next/image';
 
 type Props = {}
@@ -13,12 +15,23 @@ function Help({}: Props) {
                 <Image
                 src={OutdoorCat}
                 alt='Orange cat outdoors'
-                height={48}
-                width={48}
+                className='object-cover'
                 />
             </div>
-            <div className='bg-black col-span-1 row-span-3'>Hello</div>
-            <div className='bg-black col-span-1 row-span-3'>Hello</div>
+            <div className='bg-black col-span-1 row-span-3'>
+                <Image
+                    src={CalicoKitten}
+                    alt='Calico kitten outdoors'
+                    className='object-cover'
+                    />
+            </div>
+            <div className='bg-black col-span-1 row-span-3'>
+                <Image
+                    src={FoliageCat}
+                    alt='Cat in foliage'
+                    className='object-cover'
+                    />
+            </div>
             <div className='bg-black col-span-1 row-span-3'>Hello</div>
             <div className='bg-black col-span-1 row-span-3'>Hello</div>
             <div className='bg-black col-span-1 row-span-3'>Hello</div>
