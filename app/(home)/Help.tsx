20278128b103cf95edc85513535b4af534b13183
Joshua Carroll <carrolljosh1976@gmail.com>
2023-09-14 21:31:55 -0400

    started to implement hamburger menu in Navbar component

--- app/(home)/Help.tsx
+++ app/(home)/Help.tsx
@@ -3,6 +3,7 @@ import OutdoorCat from "../../public/assets/outdoorcat.jpg"
 import CalicoKitten from "../../public/assets/calicokitten.jpg"
 import FoliageCat from "../../public/assets/foliagecat1.jpg"
 import Image from 'next/image';
+import HelpItem from '../components/HelpItem';
 
 type Props = {}
 
@@ -11,12 +12,15 @@ function Help({}: Props) {
     <div>
         <h1 className='text-2xl uppercase'>Getting Help</h1>
         <div className='flex my-4'>
-            <div className='mx-2 rounded-md'>
+            <div className='mx-2 rounded-md group hover:bg-transparent'>
                 <Image
                 src={OutdoorCat}
                 alt='Orange cat outdoors'
                 className='object-cover h-64 w-72'
                 />
+                <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full h-full bg-neutral-50/70'>
+                    <h3 className='text-xl uppercase'>Do you need advice about Cats?</h3>
+                </div>
             </div>
             <div className=' relative mx-2 rounded-md group hover:bg-transparent'>
                 <Image
--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import CatIcon from "public/assets/cat_icon.png";
 import SocialLinks from './SocialLinks';
+import {AiOutlineHome, AiOutlineMenu,AiOutlineMail} from "react-icons/ai";
+import { SiAboutdotme } from "react-icons/si"
 
 
 type Props = {}
 
 function Navbar({}: Props) {
+  const [div,setDav] = useState(false);
+
+  const handleNav = () => {
+    setDav(!div);
+  }
   return (
     <nav className='flex justify-between items-center w-full px-10 py-4'>
         <div className='hidden sm:block'>
@@ -23,6 +32,74 @@ function Navbar({}: Props) {
         />
         <h1 className='text-2xl font-bold'>Friends of Feral Felines</h1>
       </div>
+      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+        {div ? (
+            <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+                <a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg
+                bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <AiOutlineHome size={20} />
+                    <span className='pl-4'>Home</span>
+                </a>
+                <a onClick={handleNav} href="#about" className='w-[75%] flex justify-center items-center rounded-full shadow-lg
+                bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <SiAboutdotme size={20} />
+                    <span className='pl-4'>About</span>
+                </a>
+                {/*<a onClick={handleNav} href="#skills" className='w-[75%] flex justify-center items-center rounded-full shadow-lg
+                bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <DiJavascript1 size={20} />
+                    <span className='pl-4'>Skills</span>
+                </a>
+                <a onClick={handleNav} href="#projects" className='w-[75%] flex justify-center items-center rounded-full shadow-lg
+                bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <AiOutlineProject size={20} />
+                    <span className='pl-4'>Projects</span>
+                </a>
+                <a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg
+                bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <BsPerson size={20} />
+                    <span className='pl-4'>Resume</span>
+                 </a>*/}
+                <a onClick={handleNav} href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg
+                bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <AiOutlineMail size={20} />
+                    <span className='pl-4'>Contact</span>
+                </a>
+            </div>
+         )
+         : (
+            ""
+         )
+         }
+         <div className='md:block hidden fixed top-[25%] z-10'>
+            <div className='flex flex-col'>
+                <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer
+                 hover:scale-110 ease-in duration-300'>
+                    <AiOutlineHome size={20} />
+                </a>
+                <a href="#about" className='w-[75%] flex justify-center items-center rounded-full shadow-lg
+                bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                    <SiAboutdotme size={20} />
+                    <span className='pl-4'></span>
+                </a>
+                {/*<a href="#skills" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer
+                 hover:scale-110 ease-in duration-300'>
+                    <DiJavascript1 size={20} />
+                </a>
+                <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer
+                 hover:scale-110 ease-in duration-300'>
+                    <AiOutlineProject size={20} />
+                </a>
+                {/*<a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer
+                 hover:scale-110 ease-in duration-300'>
+                    <BsPerson size={20} />
+                </a>*/}
+                <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer
+                 hover:scale-110 ease-in duration-300'>
+                    <AiOutlineMail size={20} />
+                </a>
+            </div>
+         </div>
       <div className='flex justify-between items-center gap-10'>
                 <Link href="/">Home</Link>
                 <Link href="/">About Us</Link>
