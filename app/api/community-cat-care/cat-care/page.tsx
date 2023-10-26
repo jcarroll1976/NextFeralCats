7fd7de758ad1dedd95b9dc36d9293fc37e88b02f
Joshua Carroll <carrolljosh1976@gmail.com>
2023-10-25 22:14:51 -0400

    started to implement layout for cat-care page

--- app/api/community-cat-care/cat-care/page.tsx
+++ app/api/community-cat-care/cat-care/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function CatCare() {
   return (
-    <div>CatCare</div>
+    <div className='flex items-center justify-center'>
+      <div>
+        <div className='w-[900px] h-[1100px] bg-blue-400'>
+          <div className='flex'>
+            <div className='w-[204px] h-[360px] bg-yellow-300'></div>
+            <div className='w-[637px] h-[1050px] bg-green-300'></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
