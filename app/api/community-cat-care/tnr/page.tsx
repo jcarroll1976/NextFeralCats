c111bbe46d69ca5dbec8bb5bc36db63aeff16e01
Joshua Carroll <carrolljosh1976@gmail.com>
2023-10-27 20:21:12 -0400

    added first list menu for sidebar component

--- app/api/community-cat-care/tnr/page.tsx
+++ app/api/community-cat-care/tnr/page.tsx
@@ -7,7 +7,17 @@ function TNR() {
         <div className=' w-[559px] h-[1604px] lg:[w-[900px] lg:h-[1100px] bg-blue-400'>
           <div className='flex'>
             <div className='w-[216px] h-[367px] bg-yellow-300'>
-              <h3>Categories</h3>
+              <h3 className='text-lg'>Categories</h3>
+              <div className='flex flex-col items-center'>
+                <ul>
+                  <li><h4>Trap-Neuter-Return</h4></li>
+                  <li>Planning</li>
+                  <li>Trapping</li>
+                  <li>Recovery</li>
+                  <li>Return</li>
+                  <li>Troubleshooting</li>
+                </ul>
+              </div>
             </div>
             <div className='w-[637px] h-[1050px] bg-green-300'></div>
           </div>
